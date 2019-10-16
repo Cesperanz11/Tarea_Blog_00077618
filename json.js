d9e44acc07c7111e9d57402eff6cb01fb99bf7fd
Cesperanz11 <00077618@uca.edu.sv>
2019-10-16 17:39:52 -0600

    Terminando dia 4

--- json.js
+++ json.js
@@ -15,13 +15,54 @@ if(cap=="Inicio"){
     document.getElementById("info").innerHTML = info;
 }
 if(cap=="Chapter 1: Begin"){
-    var info= `Ahahhdhdchdc`
+    var info= `El primer dia fue una sorpresa absoluta!! Yo no sabia si era una <br>
+    mala broma o una amenaza seria. No tenia ni idea de lo que se nos venia encima. <br>
+    Conocimos a nuestro primer y mayor aliado en la lucha contra Ale Rav, el cual <br>
+    se hacia llamar Kurt Rot. Despues de ver el mensaje amenazador de Ale Rav, <br>
+    recibimos el mensaje salvador de Kurt, el cual nos dio una pizca de esperanza. <br>
+    Al final de la mision Kurt nos hablo de Ada, su amor platonico que se unio a la lucha. <br>
+    Por ultimo nos dijo que fueramos al Polideportivo, sin saber que misterio nos esperaba.`
     document.getElementById("cap").innerHTML = `CHAPTER 1: BEGIN`;
     document.getElementById("info").innerHTML = info;
 }
 if(cap=="Chapter 2: O-O-O"){
-    var info= `NBDBDBDDDN`
-    document.getElementById("cap").innerHTML = cap;
+    var info= `El segundo dia ya se nos cumplió el deseo de conocer a Ada jarvis, la <br>
+    cual con su "fineza" nos puso mas en sintonía con lo que ocurría con el código <br>
+     de las bombas, y nos retó a dar un step-up para poder mejorar nuestros métodos de <br>
+     comunicación encriptado. Por cierto, no creo que a ella le guste mucho Kurt. <br>
+     Por ultimo Kurt nos guió con Renacho Melgar, el cual era el encargado del mural <br>
+     que se realizaba abajo del Edificio D, y con su ayuda conseguiriamos el nuevo mensaje.`
+    document.getElementById("cap").innerHTML = `CHAPTER 2: O-O-O`;
+    document.getElementById("info").innerHTML = info;
+}
+if(cap=="Chapter 3: Minus Monocromo"){
+    var info= `El segundo dia ya se nos cumplió el deseo de conocer a Ada jarvis, la <br>
+    cual con su "fineza" nos puso mas en sintonía con lo que ocurría con el código <br>
+     de las bombas, y nos retó a dar un step-up para poder mejorar nuestros métodos de <br>
+     comunicación encriptado. Por cierto, no creo que a ella le guste mucho Kurt. <br>
+     Por ultimo Kurt nos guió con Renacho Melgar, el cual era el encargado del mural <br>
+     que se realizaba abajo del Edificio D, y con su ayuda conseguiriamos el nuevo mensaje.`
+    document.getElementById("cap").innerHTML = `CHAPTER 2: O-O-O`;
+    document.getElementById("info").innerHTML = info;
+}
+if(cap=="Chapter 4: Cumulonimbus"){
+    var info= `El segundo dia ya se nos cumplió el deseo de conocer a Ada jarvis, la <br>
+    cual con su "fineza" nos puso mas en sintonía con lo que ocurría con el código <br>
+     de las bombas, y nos retó a dar un step-up para poder mejorar nuestros métodos de <br>
+     comunicación encriptado. Por cierto, no creo que a ella le guste mucho Kurt. <br>
+     Por ultimo Kurt nos guió con Renacho Melgar, el cual era el encargado del mural <br>
+     que se realizaba abajo del Edificio D, y con su ayuda conseguiriamos el nuevo mensaje.`
+    document.getElementById("cap").innerHTML = `CHAPTER 2: O-O-O`;
+    document.getElementById("info").innerHTML = info;
+}
+if(cap=="Chapter 5: Commit"){
+    var info= `El segundo dia ya se nos cumplió el deseo de conocer a Ada jarvis, la <br>
+    cual con su "fineza" nos puso mas en sintonía con lo que ocurría con el código <br>
+     de las bombas, y nos retó a dar un step-up para poder mejorar nuestros métodos de <br>
+     comunicación encriptado. Por cierto, no creo que a ella le guste mucho Kurt. <br>
+     Por ultimo Kurt nos guió con Renacho Melgar, el cual era el encargado del mural <br>
+     que se realizaba abajo del Edificio D, y con su ayuda conseguiriamos el nuevo mensaje.`
+    document.getElementById("cap").innerHTML = `CHAPTER 2: O-O-O`;
     document.getElementById("info").innerHTML = info;
 }
 
